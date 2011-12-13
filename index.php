f8e3071f1e06ab2eb08811a267b6c856e48e4806
Sebastian Skuse <seb@skuse.net>
2011-12-13 14:49:42 +0000

    Added info page
    Signed-off-by: Sebastian Skuse <seb@skuse.net>

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+        <head>
+                <title>ATBar code mirror</title>
+                <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
+        </head>
+        <body>
+                <br />
+                <div class="container">
+                <div class="alert-message error">
+                        <p><strong>ATBar Code mirror</strong> <?php echo $_SERVER['SERVER_ADDR']; ?></p>
+                </div>
+                </div>
+        </body>
+</html>
